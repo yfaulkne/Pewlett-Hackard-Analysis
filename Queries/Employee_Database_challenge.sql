af36382469639e1e7894dd4c2f4f247cbda7d856
Yakeemah Faulkner <yfaulkne@gmail.com>
2022-04-29 19:30:40 -0400

    completed deliverable 2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -30,3 +30,22 @@ INTO retiring_titles
 FROM unique_titles as u
 GROUP BY u.title
 ORDER BY count DESC, u.title;
+
+
+-- Start of Deliverable 2: Create Mentorship eligibility table
+
+SELECT DISTINCT ON (emp_no) e.emp_no,
+		e.first_name,
+		e.last_name,
+		e.birth_date,
+		de.from_date,
+		de.to_date,
+		t.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE ((de.to_date = '9999-01-01') AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31' ))
+ORDER BY emp_no;
