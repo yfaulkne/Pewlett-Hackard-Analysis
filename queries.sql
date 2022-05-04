e98a9debfd85c78e8872c1323c94274be4d1a29c
Yakeemah Faulkner <yfaulkne@gmail.com>
2022-05-04 18:14:37 -0400

    clean up files and add new query

--- queries.sql
+++ queries.sql
@@ -214,3 +214,74 @@ INNER JOIN retirement_info as r
 ON (ce.emp_no = r.emp_no)
 WHERE dept_name IN ('Sales', 'Development');
 SELECT * FROM retirement_info
+
+-- Create retirement titles table for deliverable 1
+
+SELECT e.emp_no,
+		e.first_name,
+		e.last_name,
+		t.title,
+		t.from_date,
+		t.to_date
+INTO retirement_titles
+FROM employees AS e
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Create unique titles using DISTINCT ON
+
+SELECT DISTINCT ON (emp_no) r.emp_no, 
+		r.first_name,
+		r.last_name,
+		r.title
+INTO unique_titles
+FROM retirement_titles AS r
+WHERE r.to_date = '9999-01-01'
+ORDER BY r.emp_no, r.to_date DESC;
+
+-- Create retiring titles table
+
+SELECT COUNT(u.title), u.title
+INTO retiring_titles
+FROM unique_titles as u
+GROUP BY u.title
+ORDER BY count DESC, u.title;
+
+
+
+-- Start of Deliverable 2: Create Mentorship eligibility table
+
+SELECT DISTINCT ON (emp_no) e.emp_no,
+		e.first_name,
+		e.last_name,
+		e.birth_date,
+		de.from_date,
+		de.to_date,
+		t.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE ((de.to_date = '9999-01-01') AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31' ))
+ORDER BY emp_no;
+
+-- New mentorship eligibilty query
+SELECT DISTINCT ON (emp_no) e.emp_no,
+		e.first_name,
+		e.last_name,
+		e.birth_date,
+		de.from_date,
+		de.to_date,
+		t.title
+INTO new_mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE ((de.to_date = '9999-01-01') AND (e.birth_date BETWEEN '1960-01-01' AND '1965-12-31' ))
+ORDER BY emp_no;
